4403748c7a18a9921b76027ab7e31e73da47e93f
yvitall <yuritsvital05@gmail.com>
2025-11-15 21:18:58 -0300

    implementacao: tailwind

---
+++ js/users.js
@@ -0,0 +1,12 @@
+const admin = [
+    {id: 1, user: "yvital", email: "17252398@esuda.edu.br", password: "17252398"},
+    {id: 2, user: "sramara", email: "17252399@esuda.edu.br", password: "17252399"},
+    {id: 3, user: "gmarques", email: "yy@esuda.edu.br", password: "172523yy"},
+    {id: 4, user: "gcosta", email: "xx@esuda.edu.br", password: "172523xx"}
+];
+
+const coletech = [
+    {id: 5, user: "fchicout", email: "fchicout@esuda.edu.br", password: "tester@coletech"},
+    {id: 6, user: "ameliara", email: "coordtech@esuda.edu.br", password: "tester@coletech"},
+    {id: 7, user: "dsavio", email: "domingosmendes@esuda.edu.br", password: "tester@coletech"}
+];
