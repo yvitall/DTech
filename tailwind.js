33510e8f58726714edb97741a0cc8c132688f290
yvitall <yuritsvital05@gmail.com>
2025-11-19 00:03:49 -0300

    atr-tailwind-initial-screen

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sgrotesk': ['Space_Grotesk', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    }
+  }
+}
